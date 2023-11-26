5f44cd81f7dc3e8918d4b7915a77dc1639e2fe7f
Varun Nadgir <theminivann@gmail.com>
2023-11-26 08:25:24 -0500

    update SocketContext

--- client/src/components/RoomMenu.js
+++ client/src/components/RoomMenu.js
@@ -3,48 +3,7 @@ import { io } from "socket.io-client";
 import GameBox from "./GameBox";
 
 export default function RoomMenu({ username }) {
-  const [showChat, setShowChat] = useState(false);
-  const [roomKey, setRoomKey] = useState("");
   const roomKeyInputRef = useRef(null);
-
-  // simple random string generator
-  function generateRandomString(length) {
-    const alphanumericCharacters =
-      "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-    let result = "";
-
-    for (let i = 0; i < length; i++) {
-      const randomIndex = Math.floor(
-        Math.random() * alphanumericCharacters.length
-      );
-      result += alphanumericCharacters.charAt(randomIndex);
-    }
-
-    return result;
-  }
-
-  // create new room
-  const createRoom = () => {
-    const roomKey = generateRandomString(12);
-    setRoomKey(roomKey);
-    setShowChat(true);
-  };
-
-  // join active room
-  const joinRoom = (enteredRoomKey) => {
-    const socket = io("http://localhost:8080");
-
-    socket.emit("check-rooms", enteredRoomKey, (isRoomExists) => {
-      if (isRoomExists) {
-        setRoomKey(enteredRoomKey);
-        setShowChat(true);
-        roomKeyInputRef.current.value = enteredRoomKey;
-        // socket.emit("new-user-join", {username, roomKey: enteredRoomKey})
-      } else {
-        alert("Room does not exist.");
-      }
-    });
-  };
 
   // function for submitting form with Enter key
   const handleKeyPress = (event) => {
@@ -59,12 +18,6 @@ export default function RoomMenu({ username }) {
   const copyToClipboard = (text) => {
     navigator.clipboard.writeText(text);
     alert("Room Key copied to clipboard!");
-  };
-
-  // leave room and chat
-  const leaveRoom = () => {
-    setRoomKey("");
-    setShowChat(false);
   };
 
   return (
--- client/src/contexts/SocketContext.js
+++ client/src/contexts/SocketContext.js
@@ -1,8 +1,24 @@
 import { useContext, createContext, useState, useEffect, useCallback } from "react";
 import { io } from "socket.io-client";
 
+function generateRandomString(length) {
+  const alphanumericCharacters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  let result = "";
+
+  for (let i = 0; i < length; i++) {
+    const randomIndex = Math.floor(
+      Math.random() * alphanumericCharacters.length
+    );
+    result += alphanumericCharacters.charAt(randomIndex);
+  }
+
+  return result;
+}
+
 export const SocketContext = createContext({
   socket: null,
+  roomKey: null,
   createRoom: () => {},
   joinRoom: () => {},
   leaveRoom: () => {}
@@ -10,6 +26,7 @@ export const SocketContext = createContext({
 
 export function SocketProvider({ children }) {
   const [socket, setSocket] = useState(null);
+  const [roomKey, setRoomKey] = useState(null);
 
   useEffect(() => {
     const newSocket = io("http://localhost:8080");
@@ -18,8 +35,30 @@ export function SocketProvider({ children }) {
     return () => newSocket.disconnect();
   }, []);
 
+  const createRoom = useCallback(() => {
+    if (socket) {
+      const room = generateRandomString(12);
+      socket.emit("create-room", {room});
+      setRoomKey(room);
+    }
+  }, [socket]);
+
+  const joinRoom = useCallback(({room}) => {
+    if (socket) {
+      socket.emit("join-room", {room});
+      setRoomKey(room);
+    }
+  }, [socket]);
+
+  const leaveRoom = useCallback(() => {
+    if (socket) {
+      socket.emit("leave-room");
+      setRoomKey(null);
+    }
+  }, [socket]);
+
   return (
-    <SocketContext.Provider value={{socket: socket}}>
+    <SocketContext.Provider value={{socket, roomKey, createRoom, joinRoom, leaveRoom}}>
       {children}
     </SocketContext.Provider>
   )
