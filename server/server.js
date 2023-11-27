727c2d47f4239f36d46ff831c7c5b02e42bc5f2d
Varun Nadgir <theminivann@gmail.com>
2023-11-27 10:47:14 -0500

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const session = require("express-session");
+const cors = require("cors");
 const helmet = require("helmet");
 const passport = require("passport");
 const http = require("http");
 const { Server } = require("socket.io");
-const cors = require("cors");
 require("./auth");
 
 // initialize express and passport
